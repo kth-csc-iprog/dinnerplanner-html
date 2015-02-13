702aecf0eb44f91f0077de54416838f79cacbab9
thaeliss <libj@kth.se>
2015-02-13 15:19:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"));
+	var exampleView = new ExampleView($("#exampleView"), model);
 
+	var controller = new Controller(model)
 });
