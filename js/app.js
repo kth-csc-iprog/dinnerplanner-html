1d850e35ada33cd8aca88025ffff0cc1e2ab95eb
wbock <willie.bock@gmail.com>
2015-02-24 10:57:49 +0100

    All-In-One html
    step 1.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-$(function() {
+$(function () {
 	//We instantiate our model
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
 	var selectDishView = new ExampleView($("#dishContent"), model);
-	var dishDetailView = new dishDetails($("#dishDescription"),model);
-	var ingredientsTableView = new ingredientsTable($("#ingredientsTable"),model);
-	var pendingMenuView = new pendingMenu($("#pendingMenu"),model);
+	var dishDetailView = new dishDetails($("#dishDescription"), model);
+	var ingredientsTableView = new ingredientsTable($("#ingredientsTable"), model);
+	var pendingMenuView = new pendingMenu($("#pendingMenu"), model);
 	var dinnerOverviewView = new dinnerOverview($("#dinnerOverview"), model);
 	var dinnerRecipiesView = new dinnerRecipies($("#dinnerRecipies"), model);
 
