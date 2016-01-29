1d5a4c30f8b4d8443b84797c4271ce4687cf6b11
mhogler <marcus.hog@hotmail.com>
2016-01-29 16:46:34 +0100

    small change
    sup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	
+	//hellos
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
 
