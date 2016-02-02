6c22f216751fdeebae433c4eeca239085151a301
mhogler <marcus.hog@hotmail.com>
2016-02-02 14:32:12 +0100

    Test
    Gjorde ett test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	//hellos
+	//hellos22222
+	//Hej Iryna
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
 
