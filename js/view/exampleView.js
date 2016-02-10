74c99b4a0eeb876c91f6941064c2587ab462c76f
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-02-10 14:10:15 +0100

    Update exampleView.js

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -7,7 +7,7 @@ var ExampleView = function (container) {
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	
-	this.numberOfGuests.html(4);
+	this.numberOfGuests.html("4");
 	
 }
  
