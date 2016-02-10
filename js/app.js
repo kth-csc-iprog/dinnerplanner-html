ccf22ce0e7eee1aca9b9cbdb6380a4d675093ebd
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-02-10 14:19:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
+	document.write(model.getNumberOfGuests()))
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
