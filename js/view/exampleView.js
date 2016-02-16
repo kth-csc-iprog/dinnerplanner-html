5827873937b995e2c7ae54937b6f2b3ce93875f1
jensbern <jensbern@kth.se>
2016-02-16 19:23:30 +0100

    Update exampleView.js

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -12,6 +12,61 @@ var ExampleView = function (container, model) {
 	this.pic = container.find("#pic");
 	this.desc = container.find("#desc");
 	
+	this.dish = container.find("#dishID");
+	this.dishBox = container.find("#dishBox");
+	this.menuPrice = container.find("#menuPrice");
+	
+	this.starterImage = container.find("#starterImage");
+	this.starterName = container.find("#starterName");
+	this.starterDescription = container.find("#starterDescription");
+	this.starterPrice = container.find("#starterPrice");
+	
+	this.mainDishImage = container.find("#mainDishImage");
+	this.mainDishName = container.find("#mainDishName");
+	this.mainDishDescription = container.find("#mainDishDescription");
+	this.mainDishPrice=container.find("#mainDishPrice");
+	
+	this.dessertImage = container.find("#dessertImage");
+	this.dessertName = container.find("#dessertName");
+	this.dessertDescription = container.find("#dessertDescription");
+	this.dessertPrice=container.find("#dessertPrice");
+	
+	
+	
+	this.numberOfGuests.html(model.getNumberOfGuests());
+	this.dish.html(model.getDish(1).name);
+	var dishList = model.getAllDishes("main dish");
+	console.log(model.getDishPrice(1));
+	var dishOutput = "";
+	for(i = 0; i < dishList.length; i ++){
+		console.log(i);
+		dishOutput += "<div class='col-md-2'><div id ='box'>"+"<div class='stretch'>"+"<img src = 'images/" +dishList[i].image +"'>"+"</div>"+"<div >"+dishList[i].name+"</div></div><div style='width: 100%'>"+"Super duper najs mat"+"</div></div>";
+	}
+	this.dishBox.html(dishOutput);
+	model.addDishToMenu(1);
+	model.addDishToMenu(100);
+	model.addDishToMenu(202);
+	var imgDesc = "";
+	var menuList = model.getFullMenu();
+	console.log(menuList);
+	imgDesc = "<img src='images/"+menuList[0].image+"'>";
+	this.starterImage.html(imgDesc);
+	this.starterDescription.html(menuList[0].description);
+	this.starterName.html(menuList[0].name);
+	this.starterPrice.html(model.getDishPrice(menuList[0].id));
+	imgDesc = "<img src='images/"+menuList[1].image+"'>";
+	this.mainDishImage.html(imgDesc)
+	this.mainDishName.html(menuList[1].name);;
+	this.mainDishDescription.html(menuList[1].description);
+	this.mainDishPrice.html(model.getDishPrice(menuList[1].id));
+	imgDesc = "<img src='images/"+menuList[2].image+"'>";
+	this.dessertImage.html(imgDesc);
+	this.dessertName.html(menuList[2].name);
+	this.dessertDescription.html(menuList[2].description);
+	this.dessertPrice.html(model.getDishPrice(menuList[2].id));
+	
+	this.menuPrice.html(model.getTotalMenuPrice());
+	
 	this.numberOfGuests.html(model.getNumberOfGuests());
 	this.dish.html(model.getDish(1));
 	
