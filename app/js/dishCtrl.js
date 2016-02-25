c17809ea8be2c8d3f37383dc307d6955a5935850
William Perkola <perkola@users.noreply.github.com>
2016-02-25 21:31:28 +0100

    Fix typo

--- app/js/dishCtrl.js
+++ app/js/dishCtrl.js
@@ -3,7 +3,7 @@
 dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
   
   // TODO in Lab 5: you need to get the dish according to the routing parameter
-  // $routingParams.paramName
+  // $routeParams.paramName
   // Check the app.js to figure out what is the paramName in this case
   
 });
