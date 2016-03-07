24a384bd7e82935516129db7f32261257704b66f
Clement <cle.hamon@gmail.com>
2016-03-07 22:38:33 +0100

    Merge searchCtrl

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -3,50 +3,24 @@
 dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
 
-
-// search func 
-
-$scope.search = function(query) {
-   $scope.status = "Searching...";
-   Dinner.DishSearch.get({title_kw:query},function(data){
-     $scope.dishes=data.Results;
-     $scope.status = "Showing " + data.Results.length + " results";
-   },function(data){
-     $scope.status = "There was an error";
-   });
- }
-
-
-
-$scope.addDishToMenu = function(dish) {
-  Dinner.addDishToMenu(dish);
- }
-
-
-$scope.numberOfGuests = function() {
-return Dinner.getNumberOfGuests();
-}
-
-
-
-  //function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
+//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
   //you can use the filter argument to filter out the dish by name or ingredient (use for search)
   //if you don't pass any filter all the dishes will be returned
-   $scope.search = function(query, filter) {
-	   $scope.searching = true;
-	   $scope.error = false;
+  $scope.search = function(query, filter) {
+  	$scope.searching = true;
+  	$scope.error = false;
 
-	   Dinner.DishSearch.get({
-	   			title_kw:query,
-	   			any_kw:filter,
-	   		 },
-	   		 function(data){
-	     $scope.dishes=data.Results;
-	     $scope.searching = false;
-	   },function(data){
-	     $scope.error = true;
-	   });
- 	}
+  	Dinner.DishSearch.get({
+  		title_kw:query,
+  		any_kw:filter,
+  	},
+  	function(data){
+  		$scope.dishes=data.Results;
+  		$scope.searching = false;
+  	},function(data){
+  		$scope.error = true;
+  	});
+  }
 
 
 
