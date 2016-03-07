96f4ab1589457899c4c079e4192bb65ab33991b0
yash434 <yashatlancs@gmail.com>
2016-03-07 16:00:21 +0100

    Added search fun

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -2,7 +2,30 @@
 // and search results
 dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
-  // TODO in Lab 5: you will need to implement a method that searchers for dishes
-  // including the case while the search is still running.
+
+
+// search func 
+
+$scope.search = function(query) {
+   $scope.status = "Searching...";
+   Dinner.DishSearch.get({title_kw:query},function(data){
+     $scope.dishes=data.Results;
+     $scope.status = "Showing " + data.Results.length + " results";
+   },function(data){
+     $scope.status = "There was an error";
+   });
+ }
+
+
+
+$scope.addDishToMenu = function(dish) {
+  Dinner.addDishToMenu(dish);
+ }
+
+
+$scope.numberOfGuests = function() {
+return Dinner.getNumberOfGuests();
+}
+
 
 });
