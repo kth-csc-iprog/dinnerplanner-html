250b853657b88ac19709ee966f0a508d27db99cb
Clement <cle.hamon@gmail.com>
2016-03-15 23:29:11 +0100

    Remove old cookie method

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -12,31 +12,6 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
     return Dinner.getNumberOfGuests();
   }
 
-// Gettting the dishAPI
-
-// $scope.getDishAPI = function(dishId) {
-//     $scope.status = "Searching..."; 
-//     Dinner.getDishAPI.get({id:dishId},function(data){ 
-//       console.log(dishId);
-//       //var cat = data.Category;
-//       //$scope.fullMenu[cat]=data;
-//       Dinner.addDishToMenu(data);
-//       $scope.status = "Showing " + data.Title;
-//     },function(data){ 
-//       $scope.status = "There was an error"; 
-//     }); 
-//   }
-
-  // var menu = {"Appetizers":"", "Main Dish":"", "Desserts":""};
-  
-// Cookies as objects?? unsure how this would work if we have some kind of key
-  //for (key in menu) {
-   // if ($cookieStore.get(key) != undefined) {
-     // console.log("cookieStore "+$cookieStore.get(key));
-     // $scope.getDishAPI($cookieStore.get(key));
-   // }
-  
- // }
 
  $scope.fullMenu = Dinner.getFullMenu();
 
