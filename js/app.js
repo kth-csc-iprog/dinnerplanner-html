47b9bab2834e47cc3e4398c0d89b889527119b95
Filip Kis <fkis@kth.se>
2018-01-22 16:21:59 +0100

    Forgotten model

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 	var model = new DinnerModel();
 	
 	// And create the instance of ExampleView
-	var exampleView = new ExampleView($("#exampleView"));
+	var exampleView = new ExampleView($("#exampleView"), model);
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
