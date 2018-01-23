8b0e61efe20e82fda6c9f635ef6748bdd12f4372
Andrew Szucs <andrew0szucs@gmail.com>
2018-01-23 15:24:28 +0100

    Connect the initial dish search results to the model

--- js/view/DishSearchView.js
+++ js/view/DishSearchView.js
@@ -7,4 +7,32 @@
 
 const createDishSearchView = (container, model) => {
   
+  const dishSearchResultsElement = container.querySelector('#dishSearchResults')
+
+  const createDishResultElement = (imageUrl, name) => {
+    const dishResultElement = document.createElement('div')
+    dishResultElement.classList.add('dishResult')
+
+    const dishResultImageContainerElement = document.createElement('div')
+    dishResultImageContainerElement.classList.add('dishResultImageContainer')
+
+    const dishResultImageElement = document.createElement('img')
+    dishResultImageElement.classList.add('dishResultImage')
+    dishResultImageElement.src = imageUrl
+
+    const dishResultNameElement = document.createElement('div')
+    dishResultNameElement.innerHTML = name
+
+    dishResultImageContainerElement.appendChild(dishResultImageElement)
+    dishResultElement.appendChild(dishResultImageContainerElement)
+    dishResultElement.appendChild(dishResultNameElement)
+
+    return dishResultElement;
+  }
+
+  model.getAllDishes('all').map(({image, name}) => {
+    dishSearchResultsElement.appendChild(createDishResultElement(`images/${image}`, name))
+  })
+
+
 }
