332a4624dfccb4ea35fe26830c90d5dbaf617d47
Kristoffer <brkristoffer@gmail.com>
2018-01-24 22:24:18 +0100

    prepared for button

--- js/view/dishoverviewView.js
+++ js/view/dishoverviewView.js
@@ -3,4 +3,8 @@ var DishOverviewView = function (container, model) {
 	var attendance = "<strong>" +(model.getNumberOfGuests()) + " </strong>people";
 	var dinnerAttendance = container.find('#dinnerAttendance');
 	dinnerAttendance.append(attendance);
+
+	var addedDishes = container.find('#addedDishes');
+
+	addedDishes.append(model.getDish(1).name);
 }
