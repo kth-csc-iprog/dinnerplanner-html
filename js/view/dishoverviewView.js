5cb6801eba42dae4a9017678909ae01519d348a9
NiekBijman <niekbijman@gmail.com>
2018-01-24 22:48:18 +0100

    Fixed merge changes

--- js/view/dishoverviewView.js
+++ js/view/dishoverviewView.js
@@ -6,20 +6,12 @@ var DishOverviewView = function (container, model) {
 	// var chosenDish = container.find('#chosenDish');
 
 	dinnerAttendance.append(attendance);
-<<<<<<< HEAD
+
 	for(var i = 1; i < 4; i++ ){
 		chosenDishes.append("'<img id='icecream.jpg' src=/images/" + model.getDish(i).image +">'<p>" + model.getDish(i).name + "</p>");
 
 	}
-	 // for (var i = 0; i < this.menu.length; i++) {
-  //     var existingDish = this.getDish(this.menu[i]);
-
-	// chosenDish.addClass('dishview');
-}
-=======
 
 	var addedDishes = container.find('#addedDishes');
-
 	addedDishes.append(model.getDish(1).name);
 }
->>>>>>> 332a4624dfccb4ea35fe26830c90d5dbaf617d47
