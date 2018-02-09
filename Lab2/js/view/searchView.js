32f59ea4db126d42a8ca86e7cd4410f5925f6afc
lindelindblad <lindelindblad@gmail.com>
2018-02-09 10:46:15 +0100

    uppdaterade knapp och hämtning av data

--- Lab2/js/view/searchView.js
+++ Lab2/js/view/searchView.js
@@ -1,19 +1,18 @@
 var SearchView = function (container, model) {
 	this.container = container;
 	this.searchButton = container.find("#searchButton");
-	//tar ut valt element
-	var element = container.find("#selectOption option:selected").val();
-	
+	this.textInput = container.find("#keywords");  	
 
-	var menu = model.getAllDishes(element);
-  	var string = '';
-  	var picBox = container.find("#template");
-
-  	//console.log(menu);
-	for(i in menu){
-		string += '<div class="col-md-4 col-sm-6"><a href="#" class="thumbnail"><img src="images/' + menu[i].image + ' " ><div class="caption"><h3>' + menu[i].name + '</h3></div></a></div>';
-	}
-
-	picBox.html(string);
-}
  
+	/*Har flyttat koden nedan till SearchViewController*/
+	/*var menu = menu;
+	var string = '';
+		var picBox = container.find("#template");
+
+		//console.log(menu);
+	for(i in menu){
+	string += '<div class="col-md-4 col-sm-6"><a href="#" class="thumbnail"><img src="images/' + menu[i].image + ' " ><div class="caption"><h3>' + menu[i].name + '</h3></div></a></div>';
+	}
+	picBox.html(string);
+	*/
+}
