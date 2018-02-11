bb4fe7474b2df9deaddebe5a50f52db06f966a40
lindelindblad <lindelindblad@gmail.com>
2018-02-11 13:19:38 +0100

    loadView() fixad och synkad m knappar
    funkade fint hela dagen, men nu hoppar den tillbaka till startsidan när
    man trycker search igen.

--- Lab2/js/controller/searchViewController.js
+++ Lab2/js/controller/searchViewController.js
@@ -6,31 +6,7 @@ var SearchViewController = function(view, model) {
 	//var searchButton = view.container.find("#searchButton");
 
 	view.searchButton.click(function() {
-		// hämta input från rutan
-		var filter = document.getElementById("keywords").value;
-		console.log(filter);
-		// hämta input från listan
-		var type = view.container.find("#selectOption option:selected").val();
-		console.log(type);
-		// mha input filtrera vad som ska visas
-		var menu = model.getAllDishes(type, filter);
-		console.log(menu);
-		// visa de rätter som gäller
-
-/*test*/
-		/*var menu = menu;
-
-		var string = '';
-  		var picBox = view.container.find("#template");
-
-  		//console.log(menu);
-		for(i in menu){
-		string += '<div class="col-md-4 col-sm-6"><a href="#" class="thumbnail"><img src="images/' + menu[i].image + ' " ><div class="caption"><h3>' + menu[i].name + '</h3></div></a></div>';
-	}
-
-	picBox.html(string);
-
-	});*/
-
-});
+		view.loadView();
+		
+	});
 }
--- Lab2/js/view/searchView.js
+++ Lab2/js/view/searchView.js
@@ -1,28 +1,37 @@
 var SearchView = function (container, model) {
 	this.container = container;
-	this.searchButton = container.find("#searchButton");
-	this.textInput = container.find("#keywords"); 
+	this.searchButton = container.find("#searchButton"); // knappen ska bara göra loadView(), i den funktionen finns resten av isntruktionerna
+	var textInput = document.getElementById("keywords"); //container.find("#keywords"); 
 	
 
 
 	//load view
 	this.loadView = function() {
-		var menu = menu;
+
+		//var type = hämta type 
+		var type = container.find("#selectOption option:selected").val();
+		//console.log(type);
+		//var keywords = Hämta filter
+		var filter = document.getElementById("keywords").value;
+		console.log(filter);
+
+		var menu = model.getAllDishes(type, filter); // stoppain filter och type i funktionen. 
+		console.log(menu);
 		var string = '';
 			var picBox = container.find("#template");
 
-			//console.log(menu);
+		//lägg in rätterna
 		for(i in menu){
 		string += '<div class="col-md-4 col-sm-6"><a href="#" class="thumbnail"><img src="images/' + menu[i].image + ' " ><div class="caption"><h3>' + menu[i].name + '</h3></div></a></div>';
 		}
 		picBox.html(string);
+		textInput.value = ""; //nollställer keywordsfönstret när man tryckt sök. 
 	}
 		
 		
 
 	// load/update view.
-	/*this.loadView();*/
+	this.loadView();
 	//attach as listener
 	model.addObserver(this);
-	console.log(model.observers);
 }
