0113d99061924bfd56b7bbf8bcaa156b9b43d785
Kristoffer <brkristoffer@gmail.com>
2018-02-16 10:49:25 +0100

    jusst commented out some stuff

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -4,7 +4,7 @@ var DinnerModel = function() {
 	//TODO Lab 1 implement the data structure that will hold number of guest
 	// and selected dishes for the dinner menu
 
-  this.guestCount = new Event(this);
+  //this.guestCount = new Event(this);
 
   var guests = 0;
   var menu = [];
