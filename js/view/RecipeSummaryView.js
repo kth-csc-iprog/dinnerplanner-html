ea4c7434483a07a9fe092644e94ff7026e65198b
Nina Laribi <ninalaribi@Bilawals-MacBook-Pro.local>
2018-02-25 21:43:28 +0100

    Added full list of new categories

--- js/view/RecipeSummaryView.js
+++ js/view/RecipeSummaryView.js
@@ -6,7 +6,7 @@
 */
 
 const createRecipeSummaryView = (container, model) => {
-  
+
   const state = {
     display: false
   }
@@ -15,12 +15,20 @@ const createRecipeSummaryView = (container, model) => {
     _remove()
 
     const menu = {
-      'starter': model.getSelectedDish('starter'),
-      'mainCourse': model.getSelectedDish('main dish'),
-      'dessert': model.getSelectedDish('dessert')
+      'appetizer': model.getSelectedDish('appetizer'),
+      'mainCourse': model.getSelectedDish('main course'),
+      'sideDish': model.getSelectedDish('side dish'),
+      'dessert': model.getSelectedDish('dessert'),
+      'salad': model.getSelectedDish('salad'),
+      'bread': model.getSelectedDish('bread'),
+      'breakfast': model.getSelectedDish('breakfast'),
+      'soup': model.getSelectedDish('soup'),
+      'beverage': model.getSelectedDish('beverage'),
+      'sauce': model.getSelectedDish('sauce'),
+      'drink': model.getSelectedDish('drink')
     }
 
-    const createRecipeElement = ({name, image, description}) => {
+    const createRecipeElement = ({title, image, description, sourceUrl, instructions}) => {
 
       const recipeElement = document.createElement('div')
       recipeElement.classList.add('recipe')
@@ -29,36 +37,40 @@ const createRecipeSummaryView = (container, model) => {
       recipeImageContainerElement.classList.add('recipeImage')
 
       const recipeImageElement = document.createElement('img')
-      recipeImageElement.src = `images/${image}`
+      recipeImageElement.src = image
       recipeImageContainerElement.appendChild(recipeImageElement)
 
       const recipeTextboxElement = document.createElement('div')
       recipeTextboxElement.classList.add('recipeTextbox')
-      
+
       const recipeNameElement = document.createElement('h2')
       recipeNameElement.classList.add('recipeName')
-      recipeNameElement.innerHTML = `${name}`
-      
+      recipeNameElement.innerHTML = `${title}`
+
       const recipeDescriptionElement = document.createElement('p')
       recipeDescriptionElement.classList.add('recipeDescriptionText')
-      recipeDescriptionElement.innerHTML = `${description}`
+      const recipeDescription = document.createElement('a')
+      recipeDescription.href = sourceUrl
+      recipeDescription.target = "_blank"
+      recipeDescription.innerHTML = "See description on original website"
+      recipeDescriptionElement.appendChild(recipeDescription)
 
       recipeTextboxElement.appendChild(recipeNameElement)
       recipeTextboxElement.appendChild(recipeDescriptionElement)
 
       const preparationTextboxElement = document.createElement('div')
       preparationTextboxElement.classList.add('recipeTextbox')
-      
+
       const preparationTitleElement = document.createElement('h3')
       preparationTitleElement.innerHTML = 'Preparation'
-      
+
       const preparationDescriptionElement = document.createElement('p')
       preparationDescriptionElement.classList.add('recipePreparationText')
-      preparationDescriptionElement.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget aliquet nibh praesent tristique. Quis hendrerit dolor magna eget. In cursus turpis massa tincidunt dui ut ornare. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Dolor sit amet consectetur adipiscing elit. Ac tincidunt vitae semper quis lectus nulla at. Mauris augue neque gravida in fermentum. Egestas diam in arcu cursus euismod quis viverra nibh. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Quis varius quam quisque id. Non odio euismod lacinia at quis risus sed vulputate. Id cursus metus aliquam eleifend mi in. Tortor pretium viverra suspendisse potenti nullam ac tortor.`
+      preparationDescriptionElement.innerHTML = instructions
 
       preparationTextboxElement.appendChild(preparationTitleElement)
       preparationTextboxElement.appendChild(preparationDescriptionElement)
-      
+
       recipeElement.appendChild(recipeImageContainerElement)
       recipeElement.appendChild(recipeTextboxElement)
       recipeElement.appendChild(preparationTextboxElement)
