2e4bce5021e5e1f4f28d7af8cd830aa398fe50dc
sofiaJagerstrom <sofia@jagerstrom.eu>
2018-02-27 21:27:51 +0100

    tog bort kommentarer

--- Lab3/js/view/searchView.js
+++ Lab3/js/view/searchView.js
@@ -7,19 +7,6 @@ var SearchView = function (container, model) {
 	var self = this;
 	var string = '';
 
-	/*var loadDishes = function(dishes) {
-		string = "";
-		var menu = dishes;
-
-		//lägg in rätterna
-		for(i in menu){
-			string += '<div class="col-md-3 col-sm-4"><div class="thumbnail picture" id="' + menu[i].id + '"><img src="https://spoonacular.com/recipeImages/' + menu[i].image + '" ><div class="caption"><h3>' + menu[i].title + '</h3></div></div></div>';	
-		}
-		//console.log(string);
-		self.picBox.html(string);
-	}*/
-
-	//load view
 	this.loadView = function() {
 
 		//var type = hämta type 
@@ -40,11 +27,7 @@ var SearchView = function (container, model) {
 		}); 
 		
 	}
-
-
-		
-		
-
+	
 	// load/update view.
 	this.loadView();
 
