ed28e795fc3af373992dc783bb64921328987725
Ella Wirstad <ella.wg@hotmail.com>
2019-01-15 22:01:39 +0100

    add sidebar view & number functions

---
+++ js/view/sideBarView.js
@@ -0,0 +1,21 @@
+var SideBarView = function (container, model) {
+
+    const numberOfGuests = container.find('#numberOfGuests');
+
+    numberOfGuests.html(model.getNumberOfGuests());
+
+    this.plusButton = container.find('#plusGuest');
+    this.minusButton = container.find('#minusGuest');
+
+    this.plusButton.on('click', function () {
+        const newNumberOfGuest = model.getNumberOfGuests() + 1;
+        model.setNumberOfGuests(newNumberOfGuest);
+    })
+
+    this.minusButton.on('click', function () {
+        const newNumberOfGuest = model.getNumberOfGuests() - 1;
+        model.setNumberOfGuests(newNumberOfGuest);
+    })
+
+
+}
