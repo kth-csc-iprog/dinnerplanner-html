782865a5e01fa919ed36df6709a70897b4ef4ae0
Ella Wirstad <ella.wg@hotmail.com>
2019-01-18 22:59:19 +0100

    add all dishes and styling

--- js/view/dishItemView.js
+++ js/view/dishItemView.js
@@ -1,12 +1,9 @@
 var DishItemView = function (container, dish) {
-    //container.appendChild('div');
     var $dishDiv = $("<div/>")  
                  .addClass("dish")
                  .addClass("column");
                  
-
     $(container).append($dishDiv);
-
     $dishDiv.append('<img src="images/' + dish.image +'"' + '/>');
     $dishDiv.append('<p class="dishName">' + dish.name + '</p>');
 }
