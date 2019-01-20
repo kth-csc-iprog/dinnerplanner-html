880f61a38ae4bf336a636fd4b00b0e43bafd4cff
Bas Straathof <bas_straathof@msn.com>
2019-01-20 12:50:00 +0100

    Create a text data object for the welcomeText in dinnerModel.js

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -184,6 +184,12 @@ var DinnerModel = function() {
     }
   }
 
+  // Welcome text
+  this.welcomeText = "\
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut  \
+          aliquip ex ea commodo consequat."
 
   // the dishes variable contains an array of all the
   // dishes in the database. Each dish has id, name, type,
