d83b7a449e8fb0b9cebc38af409bfc5c3fffffa6
Envl <yyaomingm@gmail.com>
2019-01-21 00:23:16 +0100

    printView.js finish

--- js/view/printView.js
+++ js/view/printView.js
@@ -7,32 +7,33 @@ var PrintView = function (container, model) {
     var numberOfGuests = container.find(".numberOfGuests");
     var orderedItems = container.find("#orderedItemsList");
 
-    numberOfGuests.text(model.getNumberOfGuests());
-    model.selectedDishes.forEach(e => {
-        var imgAtLeft = $("<img>");
-        var name = $("<h2/>");
-        var description = $("<p/>");
-        var preparation = $("<h3/>");
-        var prepText = $("<p/>");
-        var midPart = $("<section/>");
-        var rightPart = $("<section/>");
-        var listItem = $("<li/>");
-
-        imgAtLeft.prop("src", "../images/" + e.image);
-        name.text(e.name);
-        description.text(e.description);
-        preparation.text("PREPARATION");
-        // there is no text for prep part, so reuse description
-        prepText.text(e.description);
-
-        midPart.append(name);
-        midPart.append(description);
-        rightPart.append(preparation);
-        rightPart.append(prepText);
-        // a dish consists of 3 parts
-        listItem.append(imgAtLeft);
-        listItem.append(midPart);
-        listItem.append(rightPart);
-        orderedItems.append(listItem);
-    });
+    var loadOrderedItems = () => {
+        numberOfGuests.text(model.getNumberOfGuests());
+        model.selectedDishes.forEach(e => {
+            var imgAtLeft = $("<img>");
+            var name = $("<h2/>");
+            var description = $("<p/>");
+            var preparation = $("<h3/>");
+            var prepText = $("<p/>");
+            var midPart = $("<section/>");
+            var rightPart = $("<section/>");
+            var listItem = $("<li/>");
+            imgAtLeft.prop("src", "../images/" + e.image);
+            name.text(e.name);
+            description.text(e.description);
+            preparation.text("PREPARATION");
+            // there is no text for prep part, so reuse description
+            prepText.text(e.description);
+            midPart.append(name);
+            midPart.append(description);
+            rightPart.append(preparation);
+            rightPart.append(prepText);
+            // a dish consists of 3 parts
+            listItem.append(imgAtLeft);
+            listItem.append(midPart);
+            listItem.append(rightPart);
+            orderedItems.append(listItem);
+        });
+    }
+    loadOrderedItems();
 }
