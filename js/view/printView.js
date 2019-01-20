d7e5fb4b21163a2d5b77de320c293a8c3a35208b
GnimOay <yyaomingm@gmail.com>
2019-01-20 22:29:48 +0100

    create printView.js create dinner_printout.html

---
+++ js/view/printView.js
@@ -0,0 +1,9 @@
+/**
+ * @param {jQuery object} container - references the HTML parent element that contains the view.
+ * @param {Object} model - the reference to the Dinner Model
+ */
+
+var OverviewView = function (container, model) {
+    var numberOfGuests=container.find(".numberOfGuests");
+    
+}
