b48b024c3c1a45b697019fb078f500a3fa6b8698
Martin Lindberg <marlin5@kth.se>
2019-01-26 21:33:45 +0100

    app-ändringar
    Ändrar i app.js för att min linter säger det. kan va fel för den vill typ ta bort alla semikolon

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-window.onload= function() {
+window.onload = function () {
 	//We instantiate our model
-	const model = new DinnerModel();
-	
+  const model = new DinnerModel();
+
 	// And create the instance of ExampleView
-	const exampleView = new ExampleView(document.querySelector("#exampleView"));
+  const exampleView = new ExampleView(document.querySelector('#exampleView'));
 
-	/**
-	 * IMPORTANT: app.js is the only place where you are allowed to
-	 * query for elements in the whole document.
-	 * In other places you should limit the search only to the children 
-	 * of the specific view you're working with (see exampleView.js).
-	 */
-
-};
+/**
+* IMPORTANT: app.js is the only place where you are allowed to * query for elements in the whole document.
+ * In other places you should limit the search only to the children
+ * of the specific view you're working with (see exampleView.js).
+ */
+}
