135e3b71f84177007396b3cab1f046ddd1aa7920
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-08-25 16:34:29 +0200

    reformatting

--- src/model/dinnerModel.test.js
+++ src/model/dinnerModel.test.js
@@ -30,65 +30,65 @@ describe("DinnerModel", () => {
   describe("getting individual dishes", () => {
     it("gets the correct dish", (done) => {
       model.getDish(559251)
-        .then((data) => {
-          expect(data.title).to.equal("Breakfast Pizza");
-          done();
-        });
+      .then((data) => {
+        expect(data.title).to.equal("Breakfast Pizza");
+        done();
+      });
     }).timeout(10000);
 
     it("returns undefined if dish is not found", (done) => {
       model.getDish(-1)
-        .then((data) => {
-          expect(data.code).to.equal(404);
-          done();
-        });
+      .then((data) => {
+        expect(data.code).to.equal(404);
+        done();
+      });
     }).timeout(10000);
   });
 
   describe("filtering for dishes", () => {
     it("returns all dishes if no args are specified", (done) => {
       model.getAllDishes()
-        .then((data) => {
-          console.log("data length", data.length);
-          expect(data.length).to.equal(10);
-          done();
-        });
+      .then((data) => {
+        console.log("data length", data.length);
+        expect(data.length).to.equal(10);
+        done();
+      });
     }).timeout(10000);
 
     it("returns the correct dish type of main course and pizza", (done) => {
       model.getAllDishes("main course", "pizza")
-        .then((data) => {
-          console.log("filtered", data);
-          const onlyHasPizzas = data.every(dish => dish.title.toLowerCase().indexOf("pizza") > -1);
-          expect(onlyHasPizzas).to.equal(true);
-          done();
-        });
+      .then((data) => {
+        console.log("filtered", data);
+        const onlyHasPizzas = data.every(dish => dish.title.toLowerCase().indexOf("pizza") > -1);
+        expect(onlyHasPizzas).to.equal(true);
+        done();
+      });
     }).timeout(10000);
   });
 
   describe("menu", () => {
     it("can add dishes", (done) => {
       model.getDish(559251)
-        .then((data) => {
-          model.addDishToMenu(data);
-          expect(model.getFullMenu().length).to.equal(1);
-          expect(model.getFullMenu()[0].id).to.equal(559251);
-          done();
-        });
+      .then((data) => {
+        model.addDishToMenu(data);
+        expect(model.getFullMenu().length).to.equal(1);
+        expect(model.getFullMenu()[0].id).to.equal(559251);
+        done();
+      });
     }).timeout(10000);
 
     it("can remove dishes", (done) => {
       model.getDish(559251)
-        .then((data) => {
-          model.addDishToMenu(data);
-          expect(model.getFullMenu().length).to.equal(1);
-          expect(model.getFullMenu()[0].id).to.equal(559251);
+      .then((data) => {
+        model.addDishToMenu(data);
+        expect(model.getFullMenu().length).to.equal(1);
+        expect(model.getFullMenu()[0].id).to.equal(559251);
 
-          model.removeDishFromMenu(559251);
-          expect(model.getFullMenu().length).to.equal(0);
-          expect(model.getFullMenu()).to.not.include(data);
-          done();
-        });
+        model.removeDishFromMenu(559251);
+        expect(model.getFullMenu().length).to.equal(0);
+        expect(model.getFullMenu()).to.not.include(data);
+        done();
+      });
     }).timeout(10000);
   });
 
