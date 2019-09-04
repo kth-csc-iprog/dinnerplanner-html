646389279a34b784dfb7360e8c92e95e4f2ca25c
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2019-09-04 16:48:28 +0200

    procedural html lab2

--- src/view/overviewView.js
+++ src/view/overviewView.js
@@ -5,14 +5,25 @@ class OverviewView {
     }
 
     render() {
-        var content = /* template */ `
-        <p class="text-center p-max-width">
-            My dinner: <span class="value-num-guests">3</span> people.
-        </p>
-      </div>
-    `;
-        this.container.innerHTML = content;
-        this.afterRender();
+      const paragraph = this.container.appendChild(document.createElement('P'))
+      paragraph.innerHTML = "This dinner will be Awesome!";
+
+      const num_people_val = 3;
+      const paragraph2 = this.container.appendChild(document.createElement('P'))
+      const num_people = paragraph2.appendChild(document.createElement('SPAN'))
+      num_people.innerHTML = num_people_val;
+      paragraph2.innerHTML += " people are coming!";
+
+      const paragraph3 = this.container.appendChild(document.createElement('P'))
+      paragraph3.innerHTML = "We will be eating the following:";
+
+      const list = this.container.appendChild(document.createElement('UL'))
+
+      for(const food of ["Bread!", "Ham!", "Pizza!"]) {
+        list.appendChild(document.createElement('UL')).innerHTML = food;
+      }
+
+      this.afterRender();
     }
 
     afterRender() {
